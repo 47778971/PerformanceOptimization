3e89eb62167f18358f7efc55af04d20bfceae40a
liujun <liujun@dachentech.com.cn>
2019-04-19 11:07:45 +0800

    持续崩溃处理

--- app/src/main/java/com/jun/po/CrashCollectActivity.java
+++ app/src/main/java/com/jun/po/CrashCollectActivity.java
@@ -1,11 +1,12 @@
 package com.jun.po;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
-public class CrashCollectActivity extends FragmentActivity {
+public class CrashCollectActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,33 +15,40 @@ public class CrashCollectActivity extends FragmentActivity {
     }
 
     private void showDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("检测到应用持续崩溃");
-        builder.setMessage("请选择如下操作");
-        builder.setIcon(R.mipmap.ic_launcher_round);
-        builder.setCancelable(false);
-        builder.setPositiveButton("手动卸载", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                finish();
-            }
-        });
-        builder.setNegativeButton("清除缓存", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                finish();
-            }
-        });
-        builder.setNeutralButton("不做处理", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                finish();
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        try {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("检测到应用异常退出");
+            builder.setMessage("是否修复? ");
+            builder.setIcon(R.mipmap.ic_launcher_round);
+            builder.setCancelable(false);
+            builder.setPositiveButton("修复", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    finish();
+                }
+            });
+            builder.setNegativeButton("重新启动", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    Intent intent = new Intent(CrashCollectActivity.this, MainActivity.class);
+                    CrashCollectActivity.this.startActivity(intent);
+                    finish();
+                }
+            });
+            builder.setNeutralButton("不做处理", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    finish();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+        }
     }
 }
